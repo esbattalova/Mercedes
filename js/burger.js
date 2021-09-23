f20a85f7881a8655f95cf204e5e1d6d57d799c7a
esbattalova <77916917+esbattalova@users.noreply.github.com>
2021-09-23 22:42:28 +0500

    Модальное окно и мобильное меню

---
+++ js/burger.js
@@ -0,0 +1,18 @@
+//humburger-menu
+//menu
+const burger = document.querySelector('.humburger-menu');
+const menuItem = document.querySelector('.menu');
+const toggleMenu = () => {
+    menuItem.classList.toggle('menu-active');
+    burger.classList.toggle('humburger-menu-active');
+}
+
+burger.addEventListener('click', () => {
+    toggleMenu();
+})
+
+menuItem.addEventListener('click', () => {
+     menuItem.classList.toggle('menu-active');
+    burger.classList.toggle('humburger-menu-active');
+})
+    
